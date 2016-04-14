eef9f22d27b7971acf90d9de587ed1b3baf546aa
hackyoung <hackyoung@163.com>
2016-04-14 19:46:46 +0800

    fixed bug

--- View/default/task/editor.lpt.php
+++ View/default/task/editor.lpt.php
@@ -1,10 +1,33 @@
 <extend name="Layout.one_col">
     <fragment name="one">
+        <style>
+        .editor label {
+            width: 100px;
+            display: inline-block;
+        }
+        </style>
         <div class="leno-piece-common editor">
             <div class="input-line">
                 <input class="leno-input" placeholder="请输入标题" />
             </div>
             <div class="input-line">
+                <label>技术</label>
+            </div>
+            <div class="input-line">
+                <label>工期</label>
+                <input class="leno-input" style="width: 60px" />天
+                <input class="leno-input" style="width: 60px" />小时
+            </div>
+            <div class="input-line">
+                <label>估价</label>
+                <input class="leno-input" style="width: 100px"/>元
+            </div>
+            <div class="input-line">
+                <label>描述</label>
+                <textarea class="leno-input"></textarea>
+            </div>
+            <div class="input-line">
+                <label>需求</label>
                 <div id="editor">
                 </div>
             </div>
