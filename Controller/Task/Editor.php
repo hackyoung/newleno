835379d56689efb9eedf9dd41e8fbc2b02b529c4
hackyoung <hackyoung@163.com>
2016-05-03 21:19:50 +0800

    fixed bug

--- Controller/Task/Editor.php
+++ Controller/Task/Editor.php
@@ -5,6 +5,14 @@ class Editor extends \Controller\App
 {
     public function index()
     {
+		$task_id = $this->input('task_id', ['type' => 'uuid', 'required' => false]);
+		try {
+			$task_id && $task = \Model\Entity\Task::findOrFail($task_id);
+		} catch(\Exception $ex) {
+			$this->response = $this->response->withStatus(400);
+			return '参数不可用';
+		}
+		$this->setTask($task);
         $this->render('task.editor');
     }
 }
